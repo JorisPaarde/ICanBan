5500a9592a074bbf4c2c08314dec612283ca8f0d
JorisPaarde <jorispaarde@gmail.com>
2021-02-23 11:41:53 +0000

    added basic settings switch

---
+++ assets/js/settings.js
@@ -0,0 +1,22 @@
+// first load
+
+
+//flip switch
+$(".settings-switch").on("click" , function(){
+    let thisSwitch = this.id;
+    if ($(this).hasClass("fa-toggle-off")){
+        $(this).removeClass("fa-toggle-off");
+        $(this).addClass("fa-toggle-on");
+        console.log(`${thisSwitch} is on.`);
+        localStorage.setItem(thisSwitch, 'on');
+    }else{
+        $(this).removeClass("fa-toggle-on");
+        $(this).addClass("fa-toggle-off");
+        console.log(`${thisSwitch} is off.`);
+        localStorage.setItem(thisSwitch, 'off');
+    }
+});
+
+// dark mode
+
+// setting columns
