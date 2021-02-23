fd3e69220ed58545b35a2dd6c6d16bcd92dfa10c
JorisPaarde <jorispaarde@gmail.com>
2021-02-23 16:05:03 +0000

    fixed settings for empty local storage

--- assets/js/page-setup.js
+++ assets/js/page-setup.js
@@ -7,18 +7,35 @@ $(document).ready(function () {
     const darkmode = localStorage.getItem("settings-switch-dark");
 
     // update site to show stored settings for darkmode
-    if (String(darkmode) == "on") {
+    if (String(darkmode) === "on") {
         $("#settings-switch-dark").removeClass("fa-toggle-off");
         $("#settings-switch-dark").addClass("fa-toggle-on");
     } else {
-
         // if it's off or null set it to off
-        localStorage.setItem("settings-switch-dark", 'off');
+        localStorage.setItem("settings-switch-dark", "off");
     }
 
     // read localstorage for all 5 columns
     for (let i = 1; i <= 5; i++) {
         var column = localStorage.getItem("settings-column-" + i);
-        console.log(column);
+
+        // check if localstorage is empty and is off in html, set localstorage to off
+        if (column === null && $(`#settings-column-${i}`).hasClass("fa-toggle-off")) {
+            localStorage.setItem(("settings-column-" + i), "off");
+
+        // check if localstorage is empty and is off in html, set localstorage to off
+        } else if (column === null && $(`#settings-column-${i}`).hasClass("fa-toggle-on")) {
+            localStorage.setItem(("settings-column-" + i), "on");
+        } 
+        // if local storage says it's on, set html to that state
+        if (String(column) === "on") {
+            $("#settings-column-" + i).removeClass("fa-toggle-off");
+            $("#settings-column-" + i).addClass("fa-toggle-on");
+        }
+        // if local storage says it's off, set html to that state
+        if (String(column) === "off") {
+            $("#settings-column-" + i).addClass("fa-toggle-off");
+            $("#settings-column-" + i).removeClass("fa-toggle-on");
+        }
     }
 });
