a43a8b95bde6d8f1299347294ddfb40229bd4571
JorisPaarde <jorispaarde@gmail.com>
2021-03-04 15:09:29 +0000

    added saving columnstatus

---
+++ assets/js/populate-mem.js
@@ -0,0 +1,27 @@
+// Constructor function for columns
+function column(text, status) {
+    this.columnText = text;
+    this.columnStatus = status;
+}
+
+//populate local storage if empty
+function populateMem() {
+    localStorage.clear();
+    if (localStorage.length < 1) {
+        localStorage.setItem('darkmode', 'off');
+        // Create five column objects
+        var column1 = new column("To Do", "on");
+        var column2 = new column("Doing", "on");
+        var column3 = new column("Done", "on");
+        var column4 = new column("", "off");
+        var column5 = new column("", "off");
+        localStorage.setItem('column1', JSON.stringify(column1));
+        localStorage.setItem('column2', JSON.stringify(column2));
+        localStorage.setItem('column3', JSON.stringify(column3));
+        localStorage.setItem('column4', JSON.stringify(column4));
+        localStorage.setItem('column5', JSON.stringify(column5));
+        console.log('memory reset');
+    }
+};
+
+populateMem();
--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,66 +1,49 @@
-//populate local storage if empty
-$(document).ready(function () {
-    if (localStorage.length < 1) {
-        localStorage.setItem('darkmode', 'off');
-        // Constructor function for columns
-        function column(text, status) {
-            this.columnText = text;
-            this.columnStatus = status;
-        }
-        // Create five column objects
-        var column1 = new column("To Do", "on");
-        var column2 = new column("Doing", "on");
-        var column3 = new column("Done", "on");
-        var column4 = new column("", "off");
-        var column5 = new column("", "off");
-
-        localStorage.setItem('column1', JSON.stringify(column1));
-        localStorage.setItem('column2', JSON.stringify(column2));
-        localStorage.setItem('column3', JSON.stringify(column3));
-        localStorage.setItem('column4', JSON.stringify(column4));
-        localStorage.setItem('column5', JSON.stringify(column5));
-    }
-});
-
 //https://www.tutorialrepublic.com/faq/how-to-check-a-checkbox-is-checked-or-not-using-jquery.php//
 
-
 //handle switch settings in localstorage and toggle darkmode
-$(document).ready(function () {
+$(document).ready(
+    checkSwitches()
+);
+
+function setColumnStatus(thisColumn, status) {
+    // get columndata and change to json format
+    let column = localStorage.getItem(thisColumn);
+    column = JSON.parse(column);
+    // set columnstatus to this status
+    column.columnStatus = status;
+    // convert JSON back to sting
+    column = JSON.stringify(column);
+    // store the new data for this column
+    localStorage.setItem(thisColumn, column);
+};
+
+function checkSwitches() {
     $('.column , input[type="checkbox"]').click(function () {
         //get id of the switch that's clicked
         let thisSwitch = $(this).parent().siblings()[1].id;
         if ($(this).prop("checked") == true) {
             // if it's darkmode update css
-            if (thisSwitch == 'darkmode'){
+            if (thisSwitch == 'darkmode') {
                 document.documentElement.setAttribute('darkmode', 'on');
                 //save this setting
                 localStorage.setItem(thisSwitch, 'on');
-            }else{
-                // this is a column turn it on and save the name
-                let columnName = $(this).parent().siblings()[1].innerHTML;
-                localStorage.setItem(thisSwitch, columnName);
-                console.log(thisSwitch);
-                console.log(column1);
-                console.log(column2);
-                if(thisSwitch == column1){
-                    console.log('on');
-                }
+                $(this).parent().siblings()[1].innerHTML = "on";
+            } else {
+                // this is a column turn it on
+                setColumnStatus(thisSwitch, "on");
             }
         }
         else if ($(this).prop("checked") == false) {
             // if it's darkmode update css
-            if (thisSwitch == 'darkmode'){
+            if (thisSwitch == 'darkmode') {
                 document.documentElement.setAttribute('darkmode', 'off');
                 //save this setting
                 localStorage.setItem(thisSwitch, 'off');
-            }else{
+                $(this).parent().siblings()[1].innerHTML = "off";
+            } else {
                 // this is a column turn it off
-                let columnName = $(this).parent().siblings()[1].innerHTML;
-                localStorage.setItem(thisSwitch, columnName);
+                setColumnStatus(thisSwitch, "off");
             }
         }
-    });
-});
-
-//edit column names
+    })
+};
